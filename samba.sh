d12b270f459c1596af7c6404bc72410200ac27b4
rrube <rrubenbuenaventura@gmail.com>
2025-02-17 02:45:25 +0100

    added config to samba

--- samba.sh
+++ samba.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-apk add samba
+apk add samba --no-cache
+dialog --yesno "Samba installed!. Do you want to configure it now?" 30 30
+config=$?
+if [[ $config -eq 0 ]]; then
+    dialog --inputbox "Enter the name of the shared folder: " 30 30 
+    path_folder=$?
+    mkdir -p $path_folder
